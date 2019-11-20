b50c34e59aed891dbe21d97ce069bca734298990
cao-weiwei <caoweiwei1987@aliyun.com>
2019-11-20 12:43:36 -0500

    app.py and SentimentModule.py changed
    Following things are modified:
    - app.py: remove useless lines of code, these lines for testing flask syntax
    - SentimentModule.py: modify the positive and negative probability and add some test cases

--- TwitterDataExtraction/SentimentModule.py
+++ TwitterDataExtraction/SentimentModule.py
@@ -17,10 +17,18 @@ class SentimentModule:
                 # text_tb = TextBlob(tweet.text, analyzer=NaiveBayesAnalyzer())
                 text_tb = TextBlob(tweet.text)
                 # print("positive------- = {}".format(text_tb.sentiment.p_pos))
-                print("positive------- = {}".format(text_tb.sentiment.polarity))
+                print("polarity------- = {}".format(text_tb.sentiment.polarity))
 
-                tweet.positivePercentage = text_tb.sentiment.polarity
-                tweet.negativePercentage = text_tb.sentiment.subjectivity
+                # use sentiment.polarity to classify polarity
+                if text_tb.sentiment.polarity >= 0:
+                    tweet.positivePercentage = text_tb.sentiment.polarity * 0.5 + 0.5
+                    tweet.negativePercentage = 1 - tweet.positivePercentage
+                else:
+                    tweet.negativePercentage = -0.5 * text_tb.sentiment.polarity + 0.5
+                    tweet.positivePercentage = 1 - tweet.negativePercentage
+
+                # tweet.positivePercentage = text_tb.sentiment.polarity * flag
+                # tweet.negativePercentage = 1 - abs(tweet.positivePercentage)
                 positivePercentageSum += tweet.positivePercentage
                 negativePercentageSum += tweet.negativePercentage
             province.averageNegativePercentage = negativePercentageSum / len(province.tweets)
@@ -29,3 +37,40 @@ class SentimentModule:
 
         return tweetsByProvince
 
+
+if __name__ == '__main__':
+    # test case for sentiment polarity
+    texts = [
+        "The internet in Iran is effectively shut down, which has made it difficult to get a clear picture of " \
+        "what's happening in the country. DW verified videos from inside the country and summarized events over " \
+        "the last week. ",
+        "“How do I look loud,” B shouts at the camera in response. “How do I look loud? I’m loud? I don’t even think "
+        "I’m like loud. Suck my ass.” ",
+        "Beijing is signaling a much tougher line on Hong Kong, striking at the heart of one country, two systems.",
+        "Very disappointed! When I wear them for the first time I felt them weird like a air bubble inside the sole ! "
+        "After 3months I have a hole inside the sole of the shoes and it very inconfortable",
+        "These pair of shoes are the worst i’ve Ever got. They’re not comfy and the lining of the shoes falls apart "
+        "and it makes a very bad odour . I love Clark’s shoes but this is such a bad experience ! ",
+    ]
+    for text in texts:
+        tb_data = TextBlob(text)
+        positive = negative = None
+
+        # solution1
+        # if tb_data.polarity >= 0:
+        #     positive = tb_data.polarity
+        #     negative = 1 - positive
+        # else:
+        #     negative = -1 * tb_data.polarity
+        #     positive = 1 - negative
+
+        # solution2
+        if tb_data.polarity >= 0:
+            positive = tb_data.polarity * 0.5 + 0.5
+            negative = 1 - positive
+        elif tb_data.polarity < 0:
+            negative = tb_data.polarity * -0.5 + 0.5
+            positive = 1 - negative
+
+        print("\n{} \nPolarity: {}".format(text, tb_data.polarity))
+        print("Positive: {:.5f}\nNegative: {:.5f}\n\n".format(positive, negative))
--- TwitterDataExtraction/app.py
+++ TwitterDataExtraction/app.py
@@ -26,6 +26,7 @@ def search():
 
     return 'Nothing Happened!'
 
+
 @app.route('/zoom', methods=['GET'])
 def zoom():
     if request.method == 'GET':
@@ -43,88 +44,6 @@ def zoom():
         return j
 
     return 'Nothing Happened!'
-
-
-'''
-Query from the URL parameters: ?key=value
-'''
-#
-#
-# @app.route('/query_example')
-# def query_example():
-#     lang = request.args.get('lang')
-#     loc = request.args.get('location')
-#     return "<h2> The language is {} </h2> <h2> The location is {} </h2>".format(lang, loc)
-#
-
-'''
-Form data example
-'''
-
-
-# @app.route('/post_example', methods=['POST', 'GET'])
-# def form_example():
-#     if request.method == 'POST':
-#         name = request.form.get('nickname')
-#         school = request.form.get('school')
-#         return "<h2> submitted done!</h2> <h2> Name: {}, School: {}</h2>".format(name, school)
-#
-#     return '''<form method="post" action="">
-#                 <label for="hints">Input some words</label>
-#                 <input type="text" name="nickname">
-#                 <input type="text" name="school">
-#                 <input type="submit" name="submit" value="search">
-#               </form>
-#             '''
-
-
-'''
-Json data example
-'''
-
-
-# @app.route('/json-example', methods=['POST'])
-# def json_example():
-#     req_data = request.get_json()
-#     name = None
-#
-#     if 'name' in req_data:
-#         name = req_data['name']
-#
-#     language = req_data['language']
-#     boolean_test = req_data['boolean_test']
-#     example = req_data['example'][2]  # list one
-#     python_version = req_data['version_info']['python']  # nested json data
-#     framework = req_data['Framework']
-#
-#     return '''
-#     <h2>The name is {}.<br>
-#     The language is {}.<br>
-#     The framework is {}.<br>
-#     The python_version is {}.<br>
-#     The example is {}.<br>
-#     The boolean_test is {}.</h2>
-#     '''.format(name, language, framework, python_version, example, boolean_test)
-
-
-'''
-API example
-'''
-
-#
-# @app.route('/api-example', methods=['POST', 'GET'])
-# def api_example():
-#     if request.method == 'POST':
-#         j_data = request.get_json()
-#         data = {
-#             'name': j_data['Name'],
-#             'boolean_val': j_data['Boolean_value'][0],
-#             'version_info': j_data['Version'],
-#             'users_info': j_data['Users']['Admin'],
-#         }
-#         return jsonify(data)
-#     else:
-#         return "<h1>Test GET method!</h1>"
 
 
 if __name__ == '__main__':
